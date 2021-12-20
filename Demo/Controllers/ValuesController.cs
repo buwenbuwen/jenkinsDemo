0bc59bb5a0e25e08fbd96ef9d5ea5b7593500de5
qin <1194162794@qq.com>
2021-12-20 12:10:31 +0800

    新项目

---
+++ Demo/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("GetHtml")]
+        public string GetHtml()
+        {
+            return "出来了";
+        }
+    }
+}
